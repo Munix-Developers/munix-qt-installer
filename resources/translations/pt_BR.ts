fbe46a8c4310328c117c3c3aab3dfaba531d3fe7
MatMercer <mateusmercer@gmail.com>
2018-08-22 10:12:21 -0300

    Update pt_BR languages

--- resources/translations/pt_BR.ts
+++ resources/translations/pt_BR.ts
@@ -4,17 +4,17 @@
 <context>
     <name>LanguageSelect</name>
     <message>
-        <location filename="../../ui/languageselect.ui" line="60"/>
+        <location filename="../../forms/languageselect.ui" line="60"/>
         <source>System Language:</source>
         <translation>Língua do Sistema:</translation>
     </message>
     <message>
-        <location filename="../../ui/languageselect.ui" line="153"/>
+        <location filename="../../forms/languageselect.ui" line="137"/>
         <source>Next</source>
         <translation>Próximo</translation>
     </message>
     <message>
-        <location filename="../../src/ui/languageselect.cpp" line="42"/>
+        <location filename="../../ui/languageselect.cpp" line="44"/>
         <source>Welcome to Munix!</source>
         <translation>Bem vindo ao Munix!</translation>
     </message>
@@ -27,7 +27,7 @@
         <translation type="vanished">Bem vindo ao Munix!</translation>
     </message>
     <message>
-        <location filename="../../src/ui/languageselect.cpp" line="47"/>
+        <location filename="../../ui/languageselect.cpp" line="49"/>
         <source>This installation wizard will guide you through the installation process. Start by selecting your language below.</source>
         <translation>Este instalador irá te guiar pelo processo de instalação. Comece selecionando sua língua abaixo.</translation>
     </message>
@@ -35,17 +35,17 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../ui/mainwindow.ui" line="23"/>
+        <location filename="../../forms/mainwindow.ui" line="23"/>
         <source>Munix Installer</source>
         <translation>Instalador do Munix</translation>
     </message>
     <message>
-        <location filename="../../ui/mainwindow.ui" line="78"/>
+        <location filename="../../forms/mainwindow.ui" line="78"/>
         <source>Current Step</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../ui/mainwindow.ui" line="97"/>
+        <location filename="../../forms/mainwindow.ui" line="97"/>
         <source>Current Step detailed awesome and crazy description of what is going on.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -53,32 +53,57 @@
 <context>
     <name>PartitionSelect</name>
     <message>
-        <location filename="../../src/ui/partitionselect.ui" line="69"/>
+        <location filename="../../forms/partitionselect.ui" line="75"/>
         <source>Select how you would like to install Munix:</source>
         <translation>Selecione como instalar o Munix:</translation>
     </message>
     <message>
-        <location filename="../../src/ui/partitionselect.ui" line="88"/>
+        <location filename="../../forms/partitionselect.ui" line="94"/>
         <source>Delete everything and install Munix</source>
         <translation>Delete tudo e instale o Munix</translation>
     </message>
     <message>
-        <location filename="../../src/ui/partitionselect.ui" line="145"/>
+        <location filename="../../forms/partitionselect.ui" line="135"/>
+        <source>Select the disk where Munix will be installed:</source>
+        <translation>Selecione o disco onde o Munix será instalado:</translation>
+    </message>
+    <message>
+        <location filename="../../forms/partitionselect.ui" line="162"/>
+        <source>Re-scan partitions</source>
+        <translation>Re-escanear partições</translation>
+    </message>
+    <message>
+        <location filename="../../forms/partitionselect.ui" line="165"/>
+        <source>Re-scan</source>
+        <translation>Re-escanear</translation>
+    </message>
+    <message>
+        <location filename="../../forms/partitionselect.ui" line="218"/>
+        <source>Device</source>
+        <translation>Dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../../forms/partitionselect.ui" line="223"/>
+        <source>Size</source>
+        <translation>Tamanho</translation>
+    </message>
+    <message>
+        <location filename="../../forms/partitionselect.ui" line="262"/>
         <source>Back</source>
         <translation>Voltar</translation>
     </message>
     <message>
-        <location filename="../../src/ui/partitionselect.ui" line="180"/>
+        <location filename="../../forms/partitionselect.ui" line="300"/>
         <source>Next</source>
         <translation>Próximo</translation>
     </message>
     <message>
-        <location filename="../../src/ui/partitionselect.cpp" line="18"/>
+        <location filename="../../ui/partitionselect.cpp" line="35"/>
         <source>Installation Type</source>
         <translation>Modo de Instalação</translation>
     </message>
     <message>
-        <location filename="../../src/ui/partitionselect.cpp" line="23"/>
+        <location filename="../../ui/partitionselect.cpp" line="40"/>
         <source>Here you can select what disk partition will be used for the installation. Currently there isn&apos;t support for dual-boot.</source>
         <translation>Aqui você seleciona quais partições em seu disco irão ser usadas para a instalação. Atualmente não existe suporte ao dual-boot.</translation>
     </message>
